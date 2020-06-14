e21d0a498674eb7562fadf127b4907ae314796ef
Edwin García <spark.com.co@gmail.com>
2020-06-14 12:00:51 -0500

    Set linter and husky to run linter pre-commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,30 +1,31 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        home: path.resolve(__dirname, 'src/index.js')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].js'
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
+  entry: {
+    home: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
 
-            },
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            title: 'my coins',
-            hash: true,
-            template: path.resolve(__dirname, 'public/index.html')
-        }),
-    ]
-}
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'my coins',
+      hash: true,
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+};
